cfd28644cd604a4857ecf15fc94b81d9269d4d3e
chris <chrisb000@gmail.com>
2017-12-20 19:54:30 -0800

    liri can now post to twitter with "node liri.js post "i am a tweet"", and search via "node liri.js get "search topic"". if topic is ommited, liri's post are returned

--- liri.js
+++ liri.js
@@ -1,32 +1,95 @@
+// import { setTimeout } from "timers";
+
+
+// call modules
 var twitter = require("./keys.js");
+var Twitter = require('twitter');
 
-console.log(twitter);
 
-var cmd = process.argv[2];
 
-client.post("statuses/update", { status: 'I am a tweet' }, function (error, tweet, response) {
-    if (!error) {
-        console.log(tweet);
+setTimeout(function () {
+
+    if (liri.act === "post") {
+        liri.post(liri.input)
+    } else if (liri.act === "get") {
+        liri.get(liri.input);
     }
-});
 
-switch (cmd) {
-
-    case "my-tweets":
-
-        break;
-
-    case "spotify-this-song":
-
-        break;
+}, 1000);
 
 
-    case "movie-this":
+var liri = {
+    // get args
+    act: process.argv[2],
+    input: process.argv[3],
 
-        break;
+    // create client object
+    client: new Twitter({
+        consumer_key: twitter.twitterKeys.consumer_key,
+        consumer_secret: twitter.twitterKeys.consumer_secret,
+        access_token_key: twitter.twitterKeys.access_token_key,
+        access_token_secret: twitter.twitterKeys.access_token_secret,
+    }),
 
-    case "do-what-it-says":
+    post: function (status) {
+        liri.client.post("statuses/update", { status: status }, function (error, tweet, response) {
+            if (!error) {
+                console.log(tweet.text);
 
-        break;
+            } else {
+                console.log(error)
+            }
+        });
+    },
+    get: function (search) {
 
+        if (search) {
+
+            liri.client.get('search/tweets', { q: search }, function (error, tweets, response) {
+
+                console.log("TWEET HISTORY (NEWEST TO OLDEST)");
+
+                for (let i = 0; i < tweets.statuses.length; i++) {
+
+                    console.log("TWEET " + i + ": " + tweets.statuses[i].text);
+                }
+            });
+
+        } else {
+
+            liri.client.get('search/tweets', { q: 'notthebotuwant' }, function (error, tweets, response) {
+
+                console.log("TWEET HISTORY (NEWEST TO OLDEST)");
+
+                for (let i = 0; i < tweets.statuses.length; i++) {
+
+                    console.log("TWEET " + i + ": " + tweets.statuses[i].text);
+                }
+            });
+        }
+    }
 }
+
+console.log("liri initiated");
+
+
+// switch (cmd) {
+
+//     case "my-tweets":
+
+//         break;
+
+//     case "spotify-this-song":
+
+//         break;
+
+
+//     case "movie-this":
+
+//         break;
+
+//     case "do-what-it-says":
+
+//         break;
+
+// }
