f22ef04423202eead7b511dffb1e1976d16edab4
chris <chrisb000@gmail.com>
2017-12-20 20:40:50 -0800

    liri gets a response from spotify

--- liri.js
+++ liri.js
@@ -4,68 +4,144 @@
 // call modules
 var twitter = require("./keys.js");
 var Twitter = require('twitter');
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
 
 
 
 setTimeout(function () {
 
-    if (liri.act === "post") {
-        liri.post(liri.input)
-    } else if (liri.act === "get") {
-        liri.get(liri.input);
+    if (liri.mod === "twitter") {
+
+        if (liri.act === "post") {
+            liri.twitter.post(liri.input)
+        } else if (liri.act === "get") {
+            liri.twitter.get(liri.input);
+        } else if (liri.act === "retweet") {
+            liri.twitter.retweet(liri.input)
+        }
+    } else if (liri.mod === "spotify") {
+        if (liri.act === "search") {
+            liri.spotify.search(liri.input)
+        }
     }
 
-}, 1000);
+}, 1);
 
 
 var liri = {
+
     // get args
-    act: process.argv[2],
-    input: process.argv[3],
+    mod: process.argv[2],
+    act: process.argv[3],
+    input: process.argv[4],
 
-    // create client object
-    client: new Twitter({
-        consumer_key: twitter.twitterKeys.consumer_key,
-        consumer_secret: twitter.twitterKeys.consumer_secret,
-        access_token_key: twitter.twitterKeys.access_token_key,
-        access_token_secret: twitter.twitterKeys.access_token_secret,
-    }),
+    // twitter module
+    twitter: {
 
-    post: function (status) {
-        liri.client.post("statuses/update", { status: status }, function (error, tweet, response) {
-            if (!error) {
-                console.log(tweet.text);
+
+
+        // create client object
+        client: new Twitter({
+            consumer_key: twitter.twitterKeys.consumer_key,
+            consumer_secret: twitter.twitterKeys.consumer_secret,
+            access_token_key: twitter.twitterKeys.access_token_key,
+            access_token_secret: twitter.twitterKeys.access_token_secret,
+        }),
+
+        // posts to twitter
+        post: function (status) {
+
+            liri.twitter.client.post("statuses/update", { status: status }, function (error, tweet, response) {
+
+                if (!error) {
+                    console.log("Tweeted: " + tweet.text);
+
+                } else {
+                    console.log(error)
+                }
+            });
+        },
+
+        // search twitter
+        get: function (search) {
+
+            if (search) {
+
+                liri.twitter.client.get('search/tweets', { q: search }, function (error, tweets, response) {
+
+                    console.log("TWEET HISTORY (NEWEST TO OLDEST)");
+
+                    for (let i = 0; i < tweets.statuses.length; i++) {
+
+                        console.log("TWEET " + i + ": " + tweets.statuses[i].text);
+                    }
+                });
 
             } else {
-                console.log(error)
+
+                liri.twitter.client.get('search/tweets', { q: 'notthebotuwant' }, function (error, tweets, response) {
+
+                    console.log("TWEET HISTORY (NEWEST TO OLDEST)");
+
+                    for (let i = 0; i < tweets.statuses.length; i++) {
+
+                        console.log("TWEET " + i + ": " + tweets.statuses[i].text);
+                    }
+                });
             }
-        });
+        },
+
+        // retweet tweets by search
+        retweet: function (search) {
+
+            liri.twitter.client.get('search/tweets', { q: search }, function (error, tweets, response) {
+
+                console.log(tweets);
+
+                for (let i = 0; i < tweets.statuses.length; i++) {
+
+                    console.log("TWEET " + i + ": " + tweets.statuses[i].text);
+                    console.log("TWEET ID " + i + ": " + tweets.statuses[i].id);
+
+
+
+                    var tweetId = tweets.statuses[i].id_str;
+
+                    liri.twitter.client.post("statuses/retweet/" + tweetId, function (error, tweet, response) {
+                        if (!error) {
+                            console.log("Tweeted: " + tweet.text);
+
+                        } else {
+                            console.log(error)
+                        }
+                    });
+                }
+            });
+        }
     },
-    get: function (search) {
 
-        if (search) {
+    // spotify module
+    spotify: {
 
-            liri.client.get('search/tweets', { q: search }, function (error, tweets, response) {
+        // spotify object
+        client: new Spotify({
+            id: "b38f6fd97cc148c99b34e535e92ff2c3",
+            secret: "da417e956f93441991a4f10e3aecca8f"
+        }),
 
-                console.log("TWEET HISTORY (NEWEST TO OLDEST)");
+        search: function (search) {
 
-                for (let i = 0; i < tweets.statuses.length; i++) {
-
-                    console.log("TWEET " + i + ": " + tweets.statuses[i].text);
+            liri.spotify.client.search({
+                type: "track",
+                query: search,
+            }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
                 }
-            });
 
-        } else {
-
-            liri.client.get('search/tweets', { q: 'notthebotuwant' }, function (error, tweets, response) {
-
-                console.log("TWEET HISTORY (NEWEST TO OLDEST)");
-
-                for (let i = 0; i < tweets.statuses.length; i++) {
-
-                    console.log("TWEET " + i + ": " + tweets.statuses[i].text);
-                }
-            });
+                console.log(data);
+            })
         }
     }
 }
